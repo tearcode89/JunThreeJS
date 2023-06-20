b75886631273841cdba7a8002abc4cced6913e00
tearcode89 <125036576+tearcode89@users.noreply.github.com>
2023-06-20 14:04:09 +0900

    35.36.37. Geometry 조작 나머지 부분 정리

--- 04.Geometry(모양)/geometry/src/ex02.js
+++ 04.Geometry(모양)/geometry/src/ex02.js
@@ -50,18 +50,32 @@ export default function example() {
 
 	//console.log(geometry.attributes.position.array);
 	const positionArray = geometry.attributes.position.array;
+	const randomArray = [];
 	for (let i = 0; i < positionArray.length; i += 3) {
 		// 정점(Vertex) 한 개의 x,y,z 좌표를 랜덤으로 조정
-		positionArray[i] = positionArray[i] + (Math.random() - 0.5) * 0.2
-		positionArray[i + 1] = positionArray[i + 1] + (Math.random() - 0.5) * 0.2
-		positionArray[i + 2] = positionArray[i + 2] + (Math.random() - 0.5) * 0.2
+		// positionArray[i] = positionArray[i] + (Math.random() - 0.5) * 0.2
+		positionArray[i] += (Math.random() - 0.5) * 0.2
+		positionArray[i + 1] += (Math.random() - 0.5) * 0.2
+		positionArray[i + 2] += (Math.random() - 0.5) * 0.2
+
+		randomArray[i] = (Math.random() - 0.5) * 0.2;
+		randomArray[i+1] = (Math.random() - 0.5) * 0.2;
+		randomArray[i+2] = (Math.random() - 0.5) * 0.2;
 	}
 
 	// 그리기
 	const clock = new THREE.Clock();
 
 	function draw() {
-		const delta = clock.getDelta();
+		const time = clock.getElapsedTime() * 3
+
+		for(let i = 0; i < positionArray.length; i += 3) {
+			positionArray[i] += Math.sin(time + randomArray[i] * 10) * 0.001
+			positionArray[i + 1] += Math.sin(time + randomArray[i + 1] * 10) * 0.001
+			positionArray[i + 2] += Math.sin(time + randomArray[i + 2] * 10) * 0.001
+		}
+
+		geometry.attributes.position.needsUpdate = true;
 
 		renderer.render(scene, camera);
 		renderer.setAnimationLoop(draw);
